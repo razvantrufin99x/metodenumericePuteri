e69f9b7a89a85b0cfc4c4cbd29665b02e467743f
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-05-19 12:21:14 +0300

    Add project files.

---
+++ metodenumericePuteri/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace metodenumericePuteri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(482, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 37);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 56);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(438, 122);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(259, 13);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(217, 37);
+            this.textBox3.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(570, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(578, 344);
+            this.panel1.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(38, 184);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(438, 352);
+            this.textBox4.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(482, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 548);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ metodenumericePuteri/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace metodenumericePuteri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public double pdx(double x, double y) {
+            return Math.Pow(x, y);
+        }
+        public string textboxtext2string(ref TextBox t)
+        {
+            return  t.Text;
+            
+        }
+        public double text2double(string s)
+        {
+            return Convert.ToDouble(s);
+        }
+        public string double2string(double x)
+        {
+            return x.ToString();
+        }
+        public void string2textboxtext(ref TextBox t, string s, string mode)
+        {
+            if (mode == "+=")
+            {
+                t.Text += s;
+            }
+            else
+            {
+                t.Text = s;
+            }
+        }
+
+        public Graphics g;
+        public Pen p = new Pen(Color.Black, 1);
+        public Brush b = new SolidBrush(Color.Black);
+        public Font f;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double v = pdx(text2double(textboxtext2string(ref this.textBox1)),text2double(textboxtext2string(ref this.textBox3)));
+            string2textboxtext(ref this.textBox2, double2string(v),"");
+        }
+        public float double2float(double d)
+        { 
+            return float.Parse(d.ToString());
+        }
+
+        public void drawcerc(ref Graphics rg, float x, float y, float r)
+        {
+            rg.DrawEllipse(p, x, y, r, r);
+        }
+        public void findalldatas(int a, int b , float p, ref TextBox t )
+        {
+            for (int i = a; i < b; i++)
+            {
+                double v = pdx(i,(double)p);
+
+                string2textboxtext(ref t, double2string(v),"+=");
+                t.Text += "\r\n";
+                adddouble2list(v, ref listofvalues);
+            }
+        }
+
+        public void adddouble2list(double d, ref List<double> ld)
+        { 
+            ld.Add(d);
+        }
+
+        public List<double> listofvalues = new List<double>();
+        public void plotDoubles2graphics(ref List<double> ld, ref Graphics rg)
+        {
+            for (int i = 0; i < ld.Count; i++)
+            {
+                drawcerc(ref rg, i * 10 + 20, double2float(ld[i]), 10);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            f = new Font(this.Font, FontStyle.Bold);
+            g = this.panel1.CreateGraphics();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            findalldatas(1, 50, 2, ref this.textBox4);
+            plotDoubles2graphics(ref listofvalues, ref g);
+        }
+    }
+}
